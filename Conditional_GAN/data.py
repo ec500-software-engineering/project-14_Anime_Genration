0136d82da3d92e9073ba147e9f8b96e034e82c68
WenjieLuo2333 <43015006+WenjieLuo2333@users.noreply.github.com>
2019-05-05 23:18:44 -0400

    Update data.py

--- Conditional_GAN/data.py
+++ Conditional_GAN/data.py
@@ -1,17 +1,32 @@
 # -*- coding: utf-8 -*-
-
+#from skimage import io, transform
 import os
 import torch
 import pandas as pd
-from skimage import io, transform
+
 import numpy as np
 import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
-
+#from torch.utils.data import Dataset, DataLoader
+#from torchvision import transforms, utils
+from torchvision import transforms
 from PIL import Image
 
-from settings import *
+import torch
+
+DATA_PATH = './extra_data/images/'
+#DATA_PATH = './faces'
+resume_file = ''
+cuda = torch.cuda.is_available()
+batch_size = 64
+z_dim = 128
+tag_num = 4
+imsize = 128
+start_epoch = 0
+max_epochs = 50
+lambda_adv = tag_num
+lambda_gp = 0.5
+learning_rate = 0.0002
+
 
 class AttrDataset(Dataset):
 
